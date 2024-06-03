6048f16d96557e6989969546aca94626b7d2a5d1
maurizb <mauricio.bertei@gmail.com>
2024-06-03 22:55:21 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  var member = document.getElementById('member');
+  var skills = document.getElementById('skills');
+  var skillsMember = document.getElementById('skillsMember');
+  member.style.display = 'none';
+  skills.style.display = 'none';
+  skillsMember.style.display = 'block';
+}
